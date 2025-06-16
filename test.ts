c924fb84e6207bbd39d024faf729ce854fcd8986
chris <jasonfan1337@gmail.com>
2025-06-16 12:33:47 +0800

    v0交易学习

--- test.ts
+++ test.ts
@@ -5,47 +5,76 @@ import {
   TransactionMessage,
   VersionedTransaction,
   SystemProgram,
+  AddressLookupTableAccount,
+  AddressLookupTableProgram,
 } from "@solana/web3.js";
 import fs from "fs";
 import { isValidSolanaAddress, connection } from "./辅助功能/1.辅助功能.ts";
 import bs58 from "bs58";
 import dotenv from "dotenv";
+import { recoverNested } from "@solana/spl-token";
 
 dotenv.config();
-async function main() {
-  const fromSecretKey = bs58.decode(process.env.DEV_PRIVATEKEY1);
-  const fromWallet = Keypair.fromSecretKey(fromSecretKey);
 
-  const toAddress = new PublicKey(
-    "93rJ8i5GfqYADUuhTimK3FruQ9Fq43auPZQkAts2WdfT"
-  );
+const fromSecretKey = bs58.decode(process.env.DEV_PRIVATEKEY1);
+const fromWallet = Keypair.fromSecretKey(fromSecretKey);
 
-  //建立指令
-  const instruction = SystemProgram.transfer({
-    fromPubkey: fromWallet.publicKey,
-    toPubkey: toAddress,
-    lamports: 1000000000,
-  });
+const toAddress = new PublicKey("93rJ8i5GfqYADUuhTimK3FruQ9Fq43auPZQkAts2WdfT");
+
+async function createALT() {
+  const slot = await connection.getSlot("confirmed");
+
+  const [lookupTableInstruction, lookupTableAddress] =
+    AddressLookupTableProgram.createLookupTable({
+      authority: fromWallet.publicKey,
+      payer: fromWallet.publicKey,
+      recentSlot: slot,
+    });
+
+  console.log("lookup table address:", lookupTableAddress.toBase58());
 
   const { blockhash } = await connection.getLatestBlockhash();
 
-  //构造消息
   const messageV0 = new TransactionMessage({
     payerKey: fromWallet.publicKey,
-    instructions: [instruction],
     recentBlockhash: blockhash,
+    instructions: [lookupTableInstruction],
   }).compileToV0Message();
 
-  //构造交易
   const transaction = new VersionedTransaction(messageV0);
+
   transaction.sign([fromWallet]);
-
+  // 模拟交易
   const simulateResult = await connection.simulateTransaction(transaction);
-  console.log("模拟交易结果：",simulateResult);
+  console.log("模拟交易结果: ", simulateResult);
 
-  //发送交易
+  // 发送交易
   const signature = await connection.sendTransaction(transaction);
   console.log(`交易已发送: https://solscan.io/tx/${signature}?cluster=devnet`);
 }
 
-main().catch(console.error);
+async function main() {
+  // //建立指令
+  // const instruction = SystemProgram.transfer({
+  //   fromPubkey: fromWallet.publicKey,
+  //   toPubkey: toAddress,
+  //   lamports: 1000000000,
+  // });
+  // const { blockhash } = await connection.getLatestBlockhash();
+  // //构造消息
+  // const messageV0 = new TransactionMessage({
+  //   payerKey: fromWallet.publicKey,
+  //   instructions: [instruction],
+  //   recentBlockhash: blockhash,
+  // }).compileToV0Message();
+  // //构造交易
+  // const transaction = new VersionedTransaction(messageV0);
+  // transaction.sign([fromWallet]);
+  // const simulateResult = await connection.simulateTransaction(transaction);
+  // console.log("模拟交易结果：", simulateResult);
+  // //发送交易
+  // const signature = await connection.sendTransaction(transaction);
+  // console.log(`交易已发送: https://solscan.io/tx/${signature}?cluster=devnet`);
+}
+createALT();
+// main().catch(console.error);
